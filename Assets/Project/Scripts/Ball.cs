2add12eca6fd6ec170a899b095cdcd009857563d
Stephan Ennen [Bamboy] <bamboy360@gmail.com>
2018-04-22 15:44:52 -0700

    Ball + paddle objects (not functional)

--- Assets/Project/Scripts/Ball.cs
+++ Assets/Project/Scripts/Ball.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class Ball : MonoBehaviour 
 {
-
 	public float speed = 0.5f;
+
+	//public float 
 
 	private CircleCollider2D col;
 	void Update()
 	{
 
+		Vector2 orign;
 
+		RaycastHit2D[] data = Physics2D.RaycastAll( new Vector2(), new Vector2(), 1f );
+
+		
 
 	}
 
+	void OnCollisionEnter2D( Collision2D impact )
+	{
+
+
+
+	}
 }
