dc5e9619843889eea58e16bcf02ac61e57d68187
fluffy <fluffy@beesbuzz.biz>
2018-04-23 12:51:24 -0700

    Interactive audio mix

--- Assets/Project/Scripts/Board.cs
+++ Assets/Project/Scripts/Board.cs
@@ -7,349 +7,317 @@ using Sirenix.OdinInspector;
 public delegate void PositionDelegate(int x, int y);
 
 [Serializable]
-public class Board
-{
-	public static readonly Vector2Int OUTOFBOUNDS = new Vector2Int(int.MinValue, int.MinValue);
-	public static readonly Vector2Int ABOVE = new Vector2Int( 0,-1);
-	public static readonly Vector2Int BELOW = new Vector2Int( 0, 1);
-	public static readonly Vector2Int LEFT  = new Vector2Int(-1, 0);
-	public static readonly Vector2Int RIGHT = new Vector2Int( 1, 0);
+public class Board {
+    public static readonly Vector2Int OUTOFBOUNDS = new Vector2Int(int.MinValue, int.MinValue);
+    public static readonly Vector2Int ABOVE = new Vector2Int( 0, -1);
+    public static readonly Vector2Int BELOW = new Vector2Int( 0, 1);
+    public static readonly Vector2Int LEFT  = new Vector2Int(-1, 0);
+    public static readonly Vector2Int RIGHT = new Vector2Int( 1, 0);
 
-	/// The tiles on our board. The int represents which player owns that space. Zero is nobody
-	[ShowInInspector]
-	public int[,] tiles;
+    /// The tiles on our board. The int represents which player owns that space. Zero is nobody
+    [ShowInInspector]
+    public int[,] tiles;
 
-	/// Tiles that need to be updated in the UI.
-	public bool[,] dirtyTiles; 
+    /// Tiles that need to be updated in the UI.
+    public bool[,] dirtyTiles;
 
-	/// Will be true if any tiles are dirty.
-	public bool isDirty = false;
+    /// Will be true if any tiles are dirty.
+    public bool isDirty = false;
 
-	/// Gets the width.
-	public int width{ get{ return tiles.GetLength(0); } }
-	/// Gets the height.
-	public int height{ get{ return tiles.GetLength(1); } }
+    /// Gets the width.
+    public int width { get { return tiles.GetLength(0); } }
+    /// Gets the height.
+    public int height { get { return tiles.GetLength(1); } }
 
-	public Vector2Int blockSpawn;
+    public Vector2Int blockSpawn;
 
-	private int _highestPoint;
-	public int highestPoint{ get{ return _highestPoint; } }
+    public int highestPoint { get { return GetHighestPoint(); } }
 
-	/// Create a new board with the given size.
-	public Board( int width, int height, int fillTokens = 0 )
-	{
-		tiles = new int[width, height];
+    /// Create a new board with the given size.
+    public Board( int width, int height, int fillTokens = 0 )
+    {
+        tiles = new int[width, height];
 
-		blockSpawn = new Vector2Int( width / 2, height - 1 );
+        blockSpawn = new Vector2Int( width / 2, height - 1 );
 
-		for (int x = 0; x < width; x++) 
-		{
-			for (int y = 0; y < height; y++) 
-			{
-				tiles[x, y] = fillTokens;
-			}
-		}
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                tiles[x, y] = fillTokens;
+            }
+        }
 
-		DirtyAll();
-		ResolveDirty();
-	}
+        DirtyAll();
+        ResolveDirty();
+    }
 
-	/// Creates a copy of the given board
-	public static Board Copy( Board other )
-	{
-		Board result = new Board( other.width, other.height );
-		for (int x = 0; x < result.width; x++) 
-		{
-			for (int y = 0; y < result.height; y++) 
-			{
-				result[x, y] = other[x, y];
-			}
-		}
-			
-		return result;
-	}
-		
-	public int this[int x, int y] 
-	{
-		get{ return tiles[x, y]; }
-		set
-		{
-			if( tiles[x, y] != value )
-			{
-				tiles[x, y] = value;
-				isDirty = true;
-				dirtyTiles[x, y] = true;
-			}
-		}
-	}
+    /// Creates a copy of the given board
+    public static Board Copy( Board other )
+    {
+        Board result = new Board( other.width, other.height );
+        for (int x = 0; x < result.width; x++) {
+            for (int y = 0; y < result.height; y++) {
+                result[x, y] = other[x, y];
+            }
+        }
+
+        return result;
+    }
+
+    public int this[int x, int y]
+    {
+        get { return tiles[x, y]; }
+        set {
+            if ( tiles[x, y] != value ) {
+                tiles[x, y] = value;
+                isDirty = true;
+                dirtyTiles[x, y] = true;
+            }
+        }
+    }
 
 
-	/// Marks all blocks to update
-	public void DirtyAll()
-	{
-		dirtyTiles = new bool[width, height];
-		for (int x = 0; x < width; x++) 
-		{
-			for (int y = 0; y < height; y++) 
-			{
-				dirtyTiles[x, y] = true;
-			}
-		}
-		isDirty = true;
-	}
-	public void ClearDirty()
-	{
-		dirtyTiles = new bool[width, height];
-		for (int x = 0; x < width; x++) 
-		{
-			for (int y = 0; y < height; y++) 
-			{
-				dirtyTiles[x, y] = false;
-			}
-		}
-		isDirty = false;
-	}
-	public void ResolveDirty()
-	{
-		if( isDirty )
-		{
-			
-			if( onResolveDirty != null )
-			{
-				for (int x = 0; x < width; x++) 
-				{
-					for (int y = 0; y < height; y++)
-					{
-						if( dirtyTiles[x, y] ) //Is this tile marked to update?
-						{
-							onResolveDirty(x, y);
-						}
-						//tiles[x, y].Set( tracking[x, y] );
-					}
-				}
-			}
-				
-			if( onAfterChanged != null )
-				onAfterChanged();
+    /// Marks all blocks to update
+    public void DirtyAll()
+    {
+        dirtyTiles = new bool[width, height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                dirtyTiles[x, y] = true;
+            }
+        }
+        isDirty = true;
+    }
+    public void ClearDirty()
+    {
+        dirtyTiles = new bool[width, height];
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                dirtyTiles[x, y] = false;
+            }
+        }
+        isDirty = false;
+    }
+    public void ResolveDirty()
+    {
+        if ( isDirty ) {
 
-			ClearDirty();
-		}
-	}
-	public int GetHighestPoint()
-	{
-		int highest = 0; 
-		for (int y = 0; y < height; y++) 
-		{
-			for (int x = 0; x < width; x++) 
-			{
-				if( tiles[x,y] != 0 )
-				{
-					highest = y;
-					break;
-				}
-			}
-		}
-		_highestPoint = highest;
-		return highest;
-	}
+            if ( onResolveDirty != null ) {
+                for (int x = 0; x < width; x++) {
+                    for (int y = 0; y < height; y++) {
+                        if ( dirtyTiles[x, y] ) { //Is this tile marked to update?
+                            onResolveDirty(x, y);
+                        }
+                        //tiles[x, y].Set( tracking[x, y] );
+                    }
+                }
+            }
 
-	public void DropEmptyRows()
-	{
-		int[,] newTiles = new int[width, height];
+            if ( onAfterChanged != null )
+            { onAfterChanged(); }
+
+            ClearDirty();
+        }
+    }
+    public int GetHighestPoint()
+    {
+        for (int y = height - 1; y >= 0; y--) {
+            for (int x = 0; x < width; x++) {
+                if ( tiles[x, y] != 0 ) {
+                    return y;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void DropEmptyRows()
+    {
+        int[,] newTiles = new int[width, height];
 
 
-		bool[] emptyRows = new bool[height];
-		for (int y = 0; y < height; y++) 
-		{
-			emptyRows[y] = IsRowEmpty(y);
-		}
-			
-		int validRowIndex = 0;
-		for (int y = 0; y < height; y++) 
-		{
-			if( emptyRows[y] == false )
-			{
-				for (int x = 0; x < width; x++) 
-				{
+        bool[] emptyRows = new bool[height];
+        for (int y = 0; y < height; y++) {
+            emptyRows[y] = IsRowEmpty(y);
+        }
 
-					newTiles[x, validRowIndex] = tiles[x, y];
+        int validRowIndex = 0;
+        for (int y = 0; y < height; y++) {
+            if ( emptyRows[y] == false ) {
+                for (int x = 0; x < width; x++) {
 
-				}
+                    newTiles[x, validRowIndex] = tiles[x, y];
 
-				validRowIndex++;
-			}
+                }
 
-		}
+                validRowIndex++;
+            }
 
-		tiles = newTiles;
-		DirtyAll();
+        }
 
-		/*
+        tiles = newTiles;
+        DirtyAll();
 
-		int highest = GetHighestPoint();
+        /*
 
-		int breakout = 300;
+        int highest = GetHighestPoint();
 
-		for (int y = 0; y < highest; y++) 
-		{
-			breakout--;
+        int breakout = 300;
 
-			if( breakout <= 0 )
-			{
-				Debug.LogError("Infinite loop in dropemptyrows");
-				break;
-			}
+        for (int y = 0; y < highest; y++)
+        {
+        	breakout--;
 
-			if( IsRowEmpty( y ) )
-			{
-				ShiftDownward( y );
-				y -= 1;
-			}
-		}*/
-	}
+        	if( breakout <= 0 )
+        	{
+        		Debug.LogError("Infinite loop in dropemptyrows");
+        		break;
+        	}
 
-	bool IsRowEmpty( int y )
-	{
-		for (int x = 0; x < width; x++) 
-		{
-			if( tiles[x,y] != 0 )
-				return false;
-		}
-		return true;
-	}
+        	if( IsRowEmpty( y ) )
+        	{
+        		ShiftDownward( y );
+        		y -= 1;
+        	}
+        }*/
+    }
 
-	void ShiftDownward( int aboveRow )
-	{
-		Board copy = Copy( this );
-		for (int y = aboveRow; y < height - 1; y++) 
-		{
-			for (int x = 0; x < width; x++) 
-			{
-				copy[x,y] = tiles[x, y + 1];
-			}
-		}
-		this.tiles = copy.tiles;
-		DirtyAll();
-	}
+    bool IsRowEmpty( int y )
+    {
+        for (int x = 0; x < width; x++) {
+            if ( tiles[x, y] != 0 )
+            { return false; }
+        }
+        return true;
+    }
 
-	/// Action to take when we go through dirty tiles.
-	public PositionDelegate onResolveDirty;
-	/// Optional action to take right after the board changes.
-	public Action onAfterChanged;
+    void ShiftDownward( int aboveRow )
+    {
+        Board copy = Copy( this );
+        for (int y = aboveRow; y < height - 1; y++) {
+            for (int x = 0; x < width; x++) {
+                copy[x, y] = tiles[x, y + 1];
+            }
+        }
+        this.tiles = copy.tiles;
+        DirtyAll();
+    }
 
-	#region Position helpers
+    /// Action to take when we go through dirty tiles.
+    public PositionDelegate onResolveDirty;
+    /// Optional action to take right after the board changes.
+    public Action onAfterChanged;
 
-	public static Vector2Int GetInDirection( Board b, Vector2Int pos, Direction dir ){ return GetInDirection(b, pos.x, pos.y, dir); }
-	public static Vector2Int GetInDirection( Board b, int x, int y, Direction dir )
-	{
-		switch( dir )
-		{
-		case Direction.Above:
-			return b.GetAbove( x, y );
+    #region Position helpers
 
-		case Direction.Below:
-			return b.GetBelow( x, y );
+    public static Vector2Int GetInDirection( Board b, Vector2Int pos, Direction dir ) { return GetInDirection(b, pos.x, pos.y, dir); }
+    public static Vector2Int GetInDirection( Board b, int x, int y, Direction dir )
+    {
+        switch ( dir ) {
+        case Direction.Above:
+            return b.GetAbove( x, y );
 
-		case Direction.Left:
-			return b.GetLeft( x, y );
+        case Direction.Below:
+            return b.GetBelow( x, y );
 
-		case Direction.Right:
-			return b.GetRight( x, y );
+        case Direction.Left:
+            return b.GetLeft( x, y );
 
-		case Direction.AboveLeft:
-			return b.GetAboveLeft( x, y );
+        case Direction.Right:
+            return b.GetRight( x, y );
 
-		case Direction.AboveRight:
-			return b.GetAboveRight( x, y );
+        case Direction.AboveLeft:
+            return b.GetAboveLeft( x, y );
 
-		case Direction.BelowLeft:
-			return b.GetBelowLeft( x, y );
+        case Direction.AboveRight:
+            return b.GetAboveRight( x, y );
 
-		case Direction.BelowRight:
-			return b.GetBelowLeft( x, y );
+        case Direction.BelowLeft:
+            return b.GetBelowLeft( x, y );
 
-		default:
-			return OUTOFBOUNDS;
-		}
-	}
+        case Direction.BelowRight:
+            return b.GetBelowLeft( x, y );
 
-	/// Returns the tile that is above the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetAbove( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + ABOVE );
-	}
-	/// Returns the tile that is below the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetBelow( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + BELOW );
-	}
-	/// Returns the tile that is to the left of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetLeft( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + LEFT );
-	}
-	/// Returns the tile that is to the right of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetRight( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + RIGHT );
-	}
+        default:
+            return OUTOFBOUNDS;
+        }
+    }
 
-	/// Returns the tile that is diagonal up-left of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetAboveLeft( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + ABOVE + LEFT );
-	}
-	/// Returns the tile that is diagonal up-right of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetAboveRight( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + ABOVE + RIGHT );
-	}
-	/// Returns the tile that is diagonal down-left of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetBelowLeft( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + BELOW + LEFT );
-	}
-	/// Returns the tile that is diagonal down-left of the given position. 
-	/// Returns -1, -1 if out of bounds.
-	public Vector2Int GetBelowRight( int x, int y )
-	{
-		return OOBFilter( new Vector2Int(x, y) + BELOW + RIGHT );
-	}
+    /// Returns the tile that is above the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetAbove( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + ABOVE );
+    }
+    /// Returns the tile that is below the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetBelow( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + BELOW );
+    }
+    /// Returns the tile that is to the left of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetLeft( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + LEFT );
+    }
+    /// Returns the tile that is to the right of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetRight( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + RIGHT );
+    }
 
-	/// Converts a position to an error state if it is out of bounds.
-	private Vector2Int OOBFilter( Vector2Int pos ){ return IsInBounds( pos ) ? pos : OUTOFBOUNDS; }
+    /// Returns the tile that is diagonal up-left of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetAboveLeft( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + ABOVE + LEFT );
+    }
+    /// Returns the tile that is diagonal up-right of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetAboveRight( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + ABOVE + RIGHT );
+    }
+    /// Returns the tile that is diagonal down-left of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetBelowLeft( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + BELOW + LEFT );
+    }
+    /// Returns the tile that is diagonal down-left of the given position.
+    /// Returns -1, -1 if out of bounds.
+    public Vector2Int GetBelowRight( int x, int y )
+    {
+        return OOBFilter( new Vector2Int(x, y) + BELOW + RIGHT );
+    }
 
-	/// Returns true if the position is within our board size.
-	public bool IsInBounds( Vector2Int pos ){ return IsInBounds( pos.x, pos.y ); }
-	/// Returns true if the position is within our board size.
-	public bool IsInBounds( int x, int y )
-	{
-		if( x < 0 || x >= width || y < 0 || y >= height )
-			return false;
-		else
-			return true;
-	}
-	#endregion
+    /// Converts a position to an error state if it is out of bounds.
+    private Vector2Int OOBFilter( Vector2Int pos ) { return IsInBounds( pos ) ? pos : OUTOFBOUNDS; }
+
+    /// Returns true if the position is within our board size.
+    public bool IsInBounds( Vector2Int pos ) { return IsInBounds( pos.x, pos.y ); }
+    /// Returns true if the position is within our board size.
+    public bool IsInBounds( int x, int y )
+    {
+        if ( x < 0 || x >= width || y < 0 || y >= height )
+        { return false; }
+        else
+        { return true; }
+    }
+    #endregion
 }
 
-public enum Direction
-{
-	None = -1,
+public enum Direction {
+    None = -1,
 
-	Above,
-	Below,
-	Left,
-	Right,
+    Above,
+    Below,
+    Left,
+    Right,
 
-	AboveLeft,
-	AboveRight,
-	BelowLeft,
-	BelowRight
+    AboveLeft,
+    AboveRight,
+    BelowLeft,
+    BelowRight
 }
--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -2,211 +2,209 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour 
-{
-	private static GameManager _instance;
-	public static GameManager singleton{ get{ return _instance; } }
+public class GameManager : MonoBehaviour {
+    private static GameManager _instance;
+    public static GameManager singleton { get { return _instance; } }
 
-	public float tileGlitchTime = 0.5f;
+    public float tileGlitchTime = 0.5f;
 
-	public Queue<Block> upcomingBlocks;
+    public Queue<Block> upcomingBlocks;
 
-	public float gameUpdateSpeed = 0.8f;
-	public float holdDownMultiplier = 5f;
-	public float holdUpMultiplier = 0.5f;
+    public float gameUpdateSpeed = 0.8f;
+    public float holdDownMultiplier = 5f;
+    public float holdUpMultiplier = 0.5f;
 
-	public List<TileAnimationSet> tileSprites;
+    public List<TileAnimationSet> tileSprites;
 
-	public int boardSizeX;
-	public int boardSizeY;
+    public int boardSizeX;
+    public int boardSizeY;
 
-	public Board board;
-	public BoardUI boardRendering;
+    public Board board;
+    public BoardUI boardRendering;
 
-	public TetrisBlock fallingBlock;
+    public TetrisBlock fallingBlock;
 
-	public VectorGrid vectorMesh;
+    public VectorGrid vectorMesh;
 
-	#region Score
-	private int _score;
-	public int score{ 
-		get{ return _score; }
-		set{
-			_score = value;
-			Scoreboard.singleton.UpdateUI();
-		}
-	}
-	private float _scoreMult = 1f;
-	/// <summary>
-	/// Gets or sets the score multiplier.
-	/// </summary>
-	/// <value>The score multiplier.</value>
-	public float scoreMultiplier
-	{
-		get{ return _scoreMult; }
-		set{
-			_scoreMult = value;
-			Scoreboard.singleton.UpdateUI();
-		}
-	}
+    public AudioSource lowMusic, highMusic;
 
-	private float _scoreCombo = 0f;
-	/// <summary>
-	/// Pending score that has not been multiplied by our multiplier
-	/// </summary>
-	public float scoreCombo
-	{
-		get{ return _scoreCombo; }
-		set{
-			_scoreCombo = value;
-			Scoreboard.singleton.UpdateUI();
-		}
-	}
+    #region Score
+    private int _score;
+    public int score
+    {
+        get { return _score; }
+        set {
+            _score = value;
+            Scoreboard.singleton.UpdateUI();
+        }
+    }
+    private float _scoreMult = 1f;
+    /// <summary>
+    /// Gets or sets the score multiplier.
+    /// </summary>
+    /// <value>The score multiplier.</value>
+    public float scoreMultiplier
+    {
+        get { return _scoreMult; }
+        set {
+            _scoreMult = value;
+            Scoreboard.singleton.UpdateUI();
+        }
+    }
 
-	public void ClearedRows( int rows )
-	{
-		int s = 100 * (rows ^ 2);
-		scoreCombo += (float)s;
+    private float _scoreCombo = 0f;
+    /// <summary>
+    /// Pending score that has not been multiplied by our multiplier
+    /// </summary>
+    public float scoreCombo
+    {
+        get { return _scoreCombo; }
+        set {
+            _scoreCombo = value;
+            Scoreboard.singleton.UpdateUI();
+        }
+    }
 
-		scoreMultiplier += (float)rows * 2f;
+    public void ClearedRows( int rows )
+    {
+        int s = 100 * (rows ^ 2);
+        scoreCombo += (float)s;
 
-		rowsClearedCombo += rows;
-	}
+        scoreMultiplier += (float)rows * 2f;
 
-	public void BallHitBlock()
-	{
-		ballHitsCombo++;
-		scoreCombo += 3f;
-		scoreMultiplier += 0.2f;
-	}
+        rowsClearedCombo += rows;
+    }
 
-	private int _ballhitcombo;
-	public int ballHitsCombo
-	{
-		get{ return _ballhitcombo; }
-		set{ _ballhitcombo = value; Scoreboard.singleton.UpdateUI(); }
-	}
-	private int _rowsClearedCombo;
-	public int rowsClearedCombo
-	{
-		get{ return _rowsClearedCombo; }
-		set{ _rowsClearedCombo = value; Scoreboard.singleton.UpdateUI(); }
-	}
+    public void BallHitBlock()
+    {
+        ballHitsCombo++;
+        scoreCombo += 3f;
+        scoreMultiplier += 0.2f;
+    }
 
-	public void FinishCombo()
-	{
-		scoreCombo += ((float)ballHitsCombo * 1.2f) + ((float)rowsClearedCombo * 5f);
-		GameManager.singleton.score += Mathf.FloorToInt(scoreCombo * scoreMultiplier);
+    private int _ballhitcombo;
+    public int ballHitsCombo
+    {
+        get { return _ballhitcombo; }
+        set { _ballhitcombo = value; Scoreboard.singleton.UpdateUI(); }
+    }
+    private int _rowsClearedCombo;
+    public int rowsClearedCombo
+    {
+        get { return _rowsClearedCombo; }
+        set { _rowsClearedCombo = value; Scoreboard.singleton.UpdateUI(); }
+    }
 
-		ballHitsCombo = 0;
-		rowsClearedCombo = 0;
+    public void FinishCombo()
+    {
+        scoreCombo += ((float)ballHitsCombo * 1.2f) + ((float)rowsClearedCombo * 5f);
+        GameManager.singleton.score += Mathf.FloorToInt(scoreCombo * scoreMultiplier);
 
-		scoreCombo = 0f;
-		scoreMultiplier = 1f;
-	}
-	#endregion
+        ballHitsCombo = 0;
+        rowsClearedCombo = 0;
 
-	private float _timeMultiplier = 1f;
-	void Awake()
-	{
-		if( _instance == null )
-		{
-			_instance = this;
-		}
-		else
-		{
-			Destroy( this.gameObject );
-			return;
-		}
+        scoreCombo = 0f;
+        scoreMultiplier = 1f;
+    }
+    #endregion
 
-
-	}
-
-	void Start()
-	{
-		Block.BuildBlocks();
-		upcomingBlocks = new Queue<Block>();
-
-		board = new Board( boardSizeX, boardSizeY, 0 );
-
-		boardRendering.Initalize( board );
-		board.DirtyAll();
-
-		upcomingBlocks = new Queue<Block>();
-		upcomingBlocks.Enqueue(
-			Block.blocks[Random.Range(0, Block.blocks.Count)]
-		);
-		upcomingBlocks.Enqueue(
-			Block.blocks[Random.Range(0, Block.blocks.Count)]
-		);
-
-		fallingBlock.Initalize( Block.blocks[1] );
-
-		StartCoroutine( GameUpdater() );
-	}
-
-	void Update()
-	{
-
-		if( Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) )
-			_timeMultiplier = holdDownMultiplier;
-		else if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
-			_timeMultiplier = holdUpMultiplier;
-		else
-			_timeMultiplier = 1f;
-
-	}
-		
-	void GameUpdate()
-	{
-		if( fallingBlock.active == false )
-		{
-			fallingBlock.Initalize( upcomingBlocks.Dequeue() );
-
-			upcomingBlocks.Enqueue(
-				Block.blocks[Random.Range(0, Block.blocks.Count)]
-			);
-		}
-		else
-			fallingBlock.OnGameUpdate();
+    private float _timeMultiplier = 1f;
+    void Awake()
+    {
+        if ( _instance == null ) {
+            _instance = this;
+        } else {
+            Destroy( this.gameObject );
+            return;
+        }
 
 
+    }
 
-		/*
-		for (int x = 0; x < board.width; x++) 
-		{
-			for (int y = 0; y < board.height; y++)
-			{
-				//board[x,y] = VectorExtras.SplitChance() ? 0 : 1;
-			}
-		} */
-	}
-	public void ForceGameUpdate()
-	{
-		_timeTillUpdate = gameUpdateSpeed;
-	}
+    void Start()
+    {
+        Block.BuildBlocks();
+        upcomingBlocks = new Queue<Block>();
 
-	void LateUpdate()
-	{
-		board.ResolveDirty();
-	}
+        board = new Board( boardSizeX, boardSizeY, 0 );
 
-	private float _timeTillUpdate;
-	IEnumerator GameUpdater()
-	{
-		_timeTillUpdate = gameUpdateSpeed;
-		while( true )
-		{
-			_timeTillUpdate -= (Time.deltaTime * _timeMultiplier);
+        boardRendering.Initalize( board );
+        board.DirtyAll();
 
-			if( _timeTillUpdate <= 0f )
-			{
-				GameUpdate();
-				_timeTillUpdate = gameUpdateSpeed;
-			}
+        upcomingBlocks = new Queue<Block>();
+        upcomingBlocks.Enqueue(
+            Block.blocks[Random.Range(0, Block.blocks.Count)]
+        );
+        upcomingBlocks.Enqueue(
+            Block.blocks[Random.Range(0, Block.blocks.Count)]
+        );
 
-			yield return null;
-		}
+        fallingBlock.Initalize( Block.blocks[1] );
 
-	}
+        StartCoroutine( GameUpdater() );
+    }
+
+    void Update()
+    {
+
+        if ( Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow) )
+        { _timeMultiplier = holdDownMultiplier; }
+        else if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        { _timeMultiplier = holdUpMultiplier; }
+        else
+        { _timeMultiplier = 1f; }
+
+        // Alter the music mix based on the board's highest point
+        float mix = board.highestPoint * 1f / board.height;
+        lowMusic.volume = Mathf.Lerp(lowMusic.volume, Mathf.Sqrt(mix), Time.deltaTime);
+        highMusic.volume = Mathf.Lerp(highMusic.volume, Mathf.Sqrt(1f - mix), Time.deltaTime);
+    }
+
+    void GameUpdate()
+    {
+        if ( fallingBlock.active == false ) {
+            fallingBlock.Initalize( upcomingBlocks.Dequeue() );
+
+            upcomingBlocks.Enqueue(
+                Block.blocks[Random.Range(0, Block.blocks.Count)]
+            );
+        } else
+        { fallingBlock.OnGameUpdate(); }
+
+
+        /*
+        for (int x = 0; x < board.width; x++)
+        {
+        	for (int y = 0; y < board.height; y++)
+        	{
+        		//board[x,y] = VectorExtras.SplitChance() ? 0 : 1;
+        	}
+        } */
+    }
+    public void ForceGameUpdate()
+    {
+        _timeTillUpdate = gameUpdateSpeed;
+    }
+
+    void LateUpdate()
+    {
+        board.ResolveDirty();
+    }
+
+    private float _timeTillUpdate;
+    IEnumerator GameUpdater()
+    {
+        _timeTillUpdate = gameUpdateSpeed;
+        while ( true ) {
+            _timeTillUpdate -= (Time.deltaTime * _timeMultiplier);
+
+            if ( _timeTillUpdate <= 0f ) {
+                GameUpdate();
+                _timeTillUpdate = gameUpdateSpeed;
+            }
+
+            yield return null;
+        }
+
+    }
 }
