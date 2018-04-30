d8a321763ed856284712b61c73a4560f530f6cc6
fluffy <fluffy@beesbuzz.biz>
2018-04-29 23:42:59 -0700

    Add itch uploader script

---
+++ upload.sh
@@ -0,0 +1,6 @@
+  #!/bin/sh
+
+butler push build/windows fluffy/brickris-tetout:postjam-windows
+butler push build/osx fluffy/brickris-tetout:postjam-osx
+butler push build/webgl/bricktris fluffy/brickris-tetout:postjam-webgl
+
